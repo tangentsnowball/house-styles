d361394c3a64f9e7c08b175daa39037d466ee2ab
MrFirthy <ashley@firthyinc.com>
2015-08-19 12:02:39 +0100

    Switched gulp load plugins variable to $ for easier readability

--- gulpfile.js
+++ gulpfile.js
@@ -22,11 +22,11 @@ var basePaths = {
     };
 
 var gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')({
+    $ = require('gulp-load-plugins')({
         pattern: '*',
         camelize: true
     }),
-    browserSync = plugins.browserSync.create(),
+    browserSync = $.browserSync.create(),
     copyFiles = {
         scripts: [
             paths.scripts.src + 'vendor/**/*'
@@ -36,14 +36,14 @@ var gulp = require('gulp'),
 /* CSS - LESS */
 function processCss(inputStream, taskType) {
     return inputStream
-        .pipe(plugins.plumber())
-        .pipe(plugins.newer(paths.styles.dest))
-        .pipe(plugins.less({ paths: [plugins.path.join(__dirname, 'less', 'includes')] }))
-        .pipe(plugins.rename({suffix: '.min'}))
-        .pipe(plugins.minifyCss({advanced: false}))
+        .pipe($.plumber())
+        .pipe($.newer(paths.styles.dest))
+        .pipe($.less({ paths: [$.path.join(__dirname, 'less', 'includes')] }))
+        .pipe($.rename({suffix: '.min'}))
+        .pipe($.minifyCss({advanced: false}))
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream())
-        .pipe(plugins.notify({ message: taskType + ' task complete' }));
+        .pipe($.notify({ message: taskType + ' task complete' }));
 }
 
 gulp.task('styles', ['less:main', 'less:responsive']);
@@ -57,18 +57,18 @@ gulp.task('less:responsive', function() {
 /* JS */
 gulp.task('scripts', ['scripts:moveFiles'], function() {
   return gulp.src(paths.scripts.src + '*.js')
-    .pipe(plugins.plumber())
-    .pipe(plugins.bytediff.start())
-    .pipe(plugins.newer(paths.scripts.dest))
-    .pipe(plugins.jshint('.jshintrc'))
-    .pipe(plugins.jshint.reporter('jshint-stylish'))
-    .pipe(plugins.concat('main.js'))
-    .pipe(plugins.rename({suffix: '.min'}))
-    .pipe(plugins.uglify())
-    .pipe(plugins.bytediff.stop())
+    .pipe($.plumber())
+    .pipe($.bytediff.start())
+    .pipe($.newer(paths.scripts.dest))
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.concat('main.js'))
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.uglify())
+    .pipe($.bytediff.stop())
     .pipe(gulp.dest(paths.scripts.dest))
     .pipe(browserSync.stream())
-    .pipe(plugins.notify({ message: 'Scripts task complete' }));
+    .pipe($.notify({ message: 'Scripts task complete' }));
 });
 
 /* Move JS files that are already minified to dist/js/ folder */
@@ -80,14 +80,14 @@ gulp.task('scripts:moveFiles', function() {
 /* Images */
 gulp.task('images', function() {
   return gulp.src(paths.images.src + '**/*')
-    .pipe(plugins.plumber())
-    .pipe(plugins.bytediff.start())
-    .pipe(plugins.newer(paths.images.dest))
-    .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(plugins.bytediff.stop())
+    .pipe($.plumber())
+    .pipe($.bytediff.start())
+    .pipe($.newer(paths.images.dest))
+    .pipe($.cache($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe($.bytediff.stop())
     .pipe(gulp.dest(paths.images.dest))
     .pipe(browserSync.stream())
-    .pipe(plugins.notify({ message: 'Images task complete' }));
+    .pipe($.notify({ message: 'Images task complete' }));
 });
 
 /* BrowserSync */
@@ -110,7 +110,7 @@ gulp.task('browser-sync', ['styles', 'scripts', 'images'], function() {
 
 /* Clean up stray files */
 gulp.task('clean', function(cb) {
-    plugins.del([paths.styles.dest, paths.scripts.dest, paths.images.dest], cb)
+    $.del([paths.styles.dest, paths.scripts.dest, paths.images.dest], cb)
 });
 
 
