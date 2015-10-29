d77eb1f1a723aef71aace3444121eba53909dc41
Edward Cobbold <edward.cobbold@gmail.com>
2015-10-29 13:32:38 +0000

    main-bower-files integrated into setup

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,9 @@ var basePaths = {
         templates: {
             src: basePaths.src + '',
             dest: basePaths.dest + ''
+        },
+        bower: {
+            src: './bower_components/'
         }
     };
 
@@ -47,6 +50,20 @@ function processCss(inputStream, taskType) {
         .pipe($.notify({ message: taskType + ' task complete' }));
 }
 
+/*
+ * Accepts a gulp stream, gulp-uglify options, filename
+ * - Concat stream content, minify and write to JS src/dest
+ */
+function minifyJS (sourceStream, uglifyOptions, filename) {
+    return sourceStream
+        .pipe($.concat(filename))
+        .pipe(gulp.dest(paths.scripts.src))
+        // Minify with safe mangling, preserve licenses
+        .pipe($.uglify(uglifyOptions))
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.scripts.dest));
+} // /function minifyJS
+
 gulp.task('styles', ['less:main', 'less:responsive']);
 gulp.task('less:main', function() {
     return processCss(gulp.src(paths.styles.src + 'styles.less'), 'Styles');
@@ -57,7 +74,7 @@ gulp.task('less:responsive', function() {
 
 /* JS */
 gulp.task('scripts', ['scripts:moveFiles'], function() {
-  return gulp.src(paths.scripts.src + '*.js')
+  return gulp.src(paths.scripts.src + 'main.js')
     .pipe($.plumber(function(error) {
         $.util.log($.util.colors.red('Error (' + error.plugin + '): ' + error.message));
         this.emit('end');
@@ -66,7 +83,6 @@ gulp.task('scripts', ['scripts:moveFiles'], function() {
     .pipe($.newer(paths.scripts.dest))
     .pipe($.jshint('.jshintrc'))
     .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.concat('main.js'))
     .pipe($.rename({suffix: '.min'}))
     .pipe($.uglify())
     .pipe($.bytediff.stop())
@@ -97,16 +113,56 @@ gulp.task('images', function() {
     .pipe($.notify({ message: 'Images task complete' }));
 });
 
+/*
+ * Grab main bower files JS, package them up into vendor.js
+ */
+gulp.task('bowerTask', function(){
+    // Grab the main bower files
+    var bowerFilesJS = gulp.src($.mainBowerFiles({
+        filter: '**/*.js'
+    }), { base: paths.bower.src });
+
+    // Set up vendor filters
+    var filterHtml5shiv = 'html5shiv/**/*',
+        filterJquery    = 'jquery/**/*';
+
+    // Set up vendor streams - process each library separately
+    var streamHtml5shiv = bowerFilesJS
+            .pipe($.filter(filterHtml5shiv)),
+        streamJquery = bowerFilesJS
+            .pipe($.filter(filterJquery)),
+    // IE8 stream, for libs only included in specific IE versions (respond, html5shiv, etc.)
+        streamIE8 = minifyJS($.mergeStream(streamHtml5shiv), {
+                mangle: { keep_fnames: true },
+                preserveComments: 'license'
+            }, 'ie8.js' ),
+    // Vendor stream, for all other 3rd-party JS
+        streamVendor = minifyJS($.mergeStream(streamJquery), {
+            mangle: {
+                except: ['jQuery'],
+                keep_fnames: true
+            },
+            preserveComments: 'license'
+        }, 'vendor.js' );
+
+    /* return merged 'completed' streams to allow the task to register as done
+     * once all streams resolve
+     */
+    return $.mergeStream(streamJquery, streamIE8);
+}); // /bowerTask
+
 /* BrowserSync */
-gulp.task('browser-sync', ['styles', 'scripts', 'images'], function() {
+gulp.task('browser-sync', ['bowerTask', 'styles', 'scripts', 'images'], function() {
     browserSync.init({
         server: {
-            baseDir: "./"
-        }
-        //Use if you don't want BS to open a tab in your browser when it starts up
-        //open: false
+            baseDir: './'
+        },
+        // Proxy for when wrapping around a project's VM devserver
+        // proxy: 'localhost:8888'
+        // Use if you don't want BS to open a tab in your browser when it starts up
+        open: false
         // Will not attempt to determine your network status, assumes you're OFFLINE
-        //online: false
+        // online: false
     });
 
     gulp.watch(paths.styles.src + '*.less', ['styles']);
@@ -121,9 +177,8 @@ gulp.task('clear', function (done) {
 
 /* Clean up stray files */
 gulp.task('clean', ['clear'], function(cb) {
-    $.del([paths.styles.dest, paths.scripts.dest, paths.images.dest], cb)
+    $.del([paths.styles.dest, paths.scripts.dest, paths.images.dest], cb);
 });
-
 
 /* Default task */
 gulp.task('default', ['clean'], function() {
